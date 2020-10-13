c7239162ba69221409fe39f84ffde4483eceada0
Jude Dike <dikejude49@gmail.com>
2020-10-13 14:45:59 +0100

    cache: add cache

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -5,6 +5,12 @@ const setCache = async (key: String, value) => {
   await redis.client.set(key, value);
 };
 
+const addCache = async (key: String, value: Number) => {
+  const prev = await getCache(key);
+  value = Number(prev) + Number(value);
+  await redis.client.set(key, value);
+};
+
 const removeCache = async (key: String) => {
   await redis.client.del(key);
 };
@@ -16,6 +22,7 @@ const getCache = async (key: String) => {
 const exportCache = {
   setCache,
   removeCache,
+  addCache,
   getCache,
 };
 export default exportCache;
