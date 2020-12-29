b9d4553dde733e75884d27b89779a744c1b2eb04
c-note <58487146+dev-Cnote@users.noreply.github.com>
2020-12-29 06:38:09 +0100

    Update ensExpirationChannel.ts

--- src/showrunners/ensExpirationChannel.ts
+++ src/showrunners/ensExpirationChannel.ts
@@ -21,7 +21,7 @@ const gr = require('graphql-request')
 const { request, gql } = gr;
 
 const NETWORK_TO_MONITOR = config.web3MainnetNetwork;
-const TRIGGER_THRESHOLD_SECS = 1623066505 //60 * 60 * 24 * 7; // 7 Days
+const TRIGGER_THRESHOLD_SECS = 60 * 60 * 24 * 7; // 7 Days
 
 @Service()
 export default class EnsExpirationChannel {
