7143c9af7bebc3f218a92cb68a10b63622aad788
Jude Dike <dikejude49@gmail.com>
2021-01-20 23:22:35 +0100

    chore: initialize epns on error

--- src/api/routes/sockets/socketWeb3.ts
+++ src/api/routes/sockets/socketWeb3.ts
@@ -5,22 +5,26 @@ import { EventDispatcher, EventDispatcherInterface } from '../../../decorators/e
 let epns;
 
 function initializeEPNS(logger, eventDispatcher) {
-  epns = epnsNotify.getInteractableContracts(
-    config.web3RopstenNetwork,                                      // Network for which the interactable contract is req
-    {                                                               // API Keys
-      etherscanAPI: config.etherscanAPI,
-      infuraAPI: config.infuraAPI,
-      alchemyAPI: config.alchemyAPI
-    },
-    config.btcTickerPrivateKey,                                     // Private Key of the Wallet sending Notification
-    config.deployedContract,                                        // The contract address which is going to be used
-    config.deployedContractABI                                      // The contract abi which is going to be useds
-  );
-  // EXAMPLE
-  epns.provider.on('block', (blockNumber) => {
-    logger.info(`🐣 New block mined! -- ${blockNumber}`);
-    eventDispatcher.dispatch("newBlockMined", blockNumber)
-  })
+  try {
+    epns = epnsNotify.getInteractableContracts(
+      config.web3RopstenNetwork,                                      // Network for which the interactable contract is req
+      {                                                               // API Keys
+        etherscanAPI: config.etherscanAPI,
+        infuraAPI: config.infuraAPI,
+        alchemyAPI: config.alchemyAPI
+      },
+      config.btcTickerPrivateKey,                                     // Private Key of the Wallet sending Notification
+      config.deployedContract,                                        // The contract address which is going to be used
+      config.deployedContractABI                                      // The contract abi which is going to be useds
+    );
+    // EXAMPLE
+    epns.provider.on('block', (blockNumber) => {
+      logger.info(`🐣 New block mined! -- ${blockNumber}`);
+      eventDispatcher.dispatch("newBlockMined", blockNumber)
+    })
+  } catch (error) {
+    initializeEPNS(logger, eventDispatcher)
+  }
 }
 
 export default async (app: Router) => {
