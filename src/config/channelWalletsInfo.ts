13cba0a77d44046bbb3acbcd7f9e36a07939ff07
Jude Dike <dikejude49@gmail.com>
2021-01-25 14:31:20 +0100

    chore: readjusted wallets architecture using channelWalletsInfo

---
+++ src/config/channelWalletsInfo.ts
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  // This error should crash whole process
+
+  throw new Error("⚠️  Couldn't find .env file  ⚠️");
+}
+
+export default {
+    mainWalletPrivateKey: process.env.WALLET_TRACKER_PRIVATE_KEY,
+    wallets: [
+        {
+            ensDomainExpiryPrivateKey_1: process.env.ENS_DOMAIN_EXPIRY_PRIVATE_KEY
+        },
+        {
+            btcTickerPrivateKey_1: process.env.BTC_TICKER_PRIVATE_KEY
+        },
+        {
+            ethTickerPrivateKey_1: process.env.ETH_TICKER_PRIVATE_KEY
+        },
+        {
+            ethGasStationPrivateKey_1: process.env.ETH_GAS_STATION_PRIVATE_KEY
+        },
+        {
+            compComptrollerPrivateKey: process.env.COMP_COMPTROLLER_PRIVATE_KEY
+        },
+        {
+            walletTrackerPrivateKey: process.env.WALLET_TRACKER_PRIVATE_KEY
+        },
+        {
+            everestPrivateKey_1: process.env.EVEREST_PRIVATE_KEY
+        }
+    ]
+}
