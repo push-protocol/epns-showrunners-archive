7a0b8edc8132275e016b99d8c71002a9ab59d68f
Jude Dike <dikejude49@gmail.com>
2021-01-27 12:51:25 +0100

    changed to alchemy timeout

--- src/api/routes/sockets/socketWeb3.ts
+++ src/api/routes/sockets/socketWeb3.ts
@@ -1,29 +1,20 @@
 import { Container } from 'typedi';
 import config from '../../../config';
-import epnsNotify from '../../../helpers/epnsNotifyHelper';
+import { ethers } from 'ethers';
+// import epnsNotify from '../../../helpers/epnsNotifyHelper';
 import { EventDispatcher, EventDispatcherInterface } from '../../../decorators/eventDispatcher';
-let epns;
-
+// let epns;
 function initializeEPNS(logger, eventDispatcher) {
   try {
-    epns = epnsNotify.getInteractableContracts(
-      config.web3RopstenNetwork,                                      // Network for which the interactable contract is req
-      {                                                               // API Keys
-        etherscanAPI: config.etherscanAPI,
-        infuraAPI: config.infuraAPI,
-        alchemyAPI: config.alchemyAPI
-      },
-      config.btcTickerPrivateKey,                                     // Private Key of the Wallet sending Notification
-      config.deployedContract,                                        // The contract address which is going to be used
-      config.deployedContractABI                                      // The contract abi which is going to be useds
-    );
+    const provider = new ethers.providers.AlchemyWebSocketProvider(3, config.alchemyAPI);
     // EXAMPLE
-    epns.provider.on('block', (blockNumber) => {
+    provider.on('block', (blockNumber) => {
       logger.info(`🐣 New block mined! -- ${blockNumber}`);
       eventDispatcher.dispatch("newBlockMined", blockNumber)
     })
   } catch (error) {
-    initializeEPNS(logger, eventDispatcher)
+    console.log(error)
+    // initializeEPNS(logger, eventDispatcher)
   }
 }
 
@@ -31,9 +22,5 @@ export default async (app: Router) => {
   const logger = Container.get('logger');
   const eventDispatcher = Container.get(EventDispatcherInterface);
   initializeEPNS(logger, eventDispatcher)
-  const thirtyMins = 30 * 60 * 1000; // Thirty mins
-  setInterval(function(){
-    initializeEPNS(logger, eventDispatcher)
-  }, thirtyMins);
 }
 
