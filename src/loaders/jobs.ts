c450ae87153eb4a781c430ba776e9cf6ae4ac749
aiswaryawalter <aiswaryawalter@gmail.com>
2021-05-14 17:27:32 +0530

    More logging on showrunners

--- src/loaders/jobs.ts
+++ src/loaders/jobs.ts
@@ -62,146 +62,146 @@ export default ({ logger }) => {
 
   // 1.1 BTC TICKER CHANNEL
   schedule.scheduleJob({ start: startTime, rule: sixHourRule }, async function () {
-    logger.info('-- 🛵 Scheduling Showrunner - BTC Ticker Channel [on 6 Hours]' + new Date(Date.now()));
+    logger.info(`[${new Date(Date.now())}] -- 🛵 Scheduling Showrunner - BTC Ticker Channel [on 6 Hours]`);
     const btcTicker = Container.get(BtcTickerChannel);
     const taskName = 'BTC Ticker Fetch and sendMessageToContract()';
 
     try {
       await btcTicker.sendMessageToContract(false);
-      logger.info(`🐣 Cron Task Completed -- ${taskName}`);
+      logger.info(`[${new Date(Date.now())}] 🐣 Cron Task Completed -- ${taskName}`);
     }
     catch (err) {
-      logger.error(`❌ Cron Task Failed -- ${taskName}`);
-      logger.error(`Error Object: %o`, err);
+      logger.error(`[${new Date(Date.now())}] ❌ Cron Task Failed -- ${taskName}`);
+      logger.error(`[${new Date(Date.now())}] Error Object: %o`, err);
     }
   });
 
   // 1.2 ETH TICKER CHANNEL
   schedule.scheduleJob({ start: startTime, rule: sixHourRule }, async function () {
-    logger.info('-- 🛵 Scheduling Showrunner - ETH Ticker Channel [on 6 Hours]' + new Date(Date.now()));
+    logger.info(`[${new Date(Date.now())}] -- 🛵 Scheduling Showrunner - ETH Ticker Channel [on 6 Hours]`);
     const ethTicker = Container.get(EthTickerChannel);
     const taskName = 'ETH Ticker Fetch and sendMessageToContract()';
 
     try {
       await ethTicker.sendMessageToContract(false);
-      logger.info(`🐣 Cron Task Completed -- ${taskName}`);
+      logger.info(`[${new Date(Date.now())}] 🐣 Cron Task Completed -- ${taskName}`);
     }
     catch (err) {
-      logger.error(`❌ Cron Task Failed -- ${taskName}`);
-      logger.error(`Error Object: %o`, err);
+      logger.error(`[${new Date(Date.now())}] ❌ Cron Task Failed -- ${taskName}`);
+      logger.error(`[${new Date(Date.now())}] Error Object: %o`, err);
     }
   });
 
 
   //1.3 ENS TICKER CHANNEL
   schedule.scheduleJob({ start: startTime, rule: dailyRule }, async function () {
-    logger.info('-- 🛵 Scheduling Showrunner - ENS Domain Expiry Channel [on 24 Hours]' + new Date(Date.now()));
+    logger.info(`[${new Date(Date.now())}] -- 🛵 Scheduling Showrunner - ENS Domain Expiry Channel [on 24 Hours]`);
     const ensTicker = Container.get(EnsExpirationChannel);
     const taskName = 'ENS Domain Expiry and sendMessageToContract()';
 
     try {
       await ensTicker.sendMessageToContract(false);
-      logger.info(`🐣 Cron Task Completed -- ${taskName}`);
+      logger.info(`[${new Date(Date.now())}] 🐣 Cron Task Completed -- ${taskName}`);
     }
     catch (err) {
-      logger.error(`❌ Cron Task Failed -- ${taskName}`);
-      logger.error(`Error Object: %o`, err);
+      logger.error(`[${new Date(Date.now())}] ❌ Cron Task Failed -- ${taskName}`);
+      logger.error(`[${new Date(Date.now())}] Error Object: %o`, err);
     }
   });
 
   // 1.4.1 GAS CHANNEL
   schedule.scheduleJob({ start: startTime, rule: tenMinuteRule }, async function () {
-    logger.info('-- 🛵 Scheduling Showrunner - Gas Price Checker [on 10 minutes]' + new Date(Date.now()));
+    logger.info(`[${new Date(Date.now())}] -- 🛵 Scheduling Showrunner - Gas Price Checker [on 10 minutes]`);
     const gasTicker = Container.get(EthGasStationChannel);
     const taskName = 'Gas result and sendMessageToContract()';
 
     try {
       await gasTicker.sendMessageToContract(false);
-      logger.info(`🐣 Cron Task Completed -- ${taskName}`);
+      logger.info(`[${new Date(Date.now())}] 🐣 Cron Task Completed -- ${taskName}`);
     }
     catch (err) {
-      logger.error(`❌ Cron Task Failed -- ${taskName}`);
-      logger.error(`Error Object: %o`, err);
+      logger.error(`[${new Date(Date.now())}] ❌ Cron Task Failed -- ${taskName}`);
+      logger.error(`[${new Date(Date.now())}] Error Object: %o`, err);
     }
   });
 
   // 1.4.2 GAS CHANNEL
   schedule.scheduleJob({ start: startTime, rule: dailyRule }, async function () {
-    logger.info('-- 🛵 Scheduling Showrunner - Gas Average Update [on 24 hours]' + new Date(Date.now()));
+    logger.info(`[${new Date(Date.now())}] -- 🛵 Scheduling Showrunner - Gas Average Update [on 24 hours]`);
     const gasDbTicker = Container.get(EthGasStationChannel);
     const taskName = 'updated mongoDb';
 
     try {
       await gasDbTicker.updateGasPriceAverage();
-      logger.info(`🐣 Cron Task Completed -- ${taskName}`);
+      logger.info(`[${new Date(Date.now())}] 🐣 Cron Task Completed -- ${taskName}`);
     }
     catch (err) {
-      logger.error(`❌ Cron Task Failed -- ${taskName}`);
-      logger.error(`Error Object: %o`, err);
+      logger.error(`[${new Date(Date.now())}] ❌ Cron Task Failed -- ${taskName}`);
+      logger.error(`[${new Date(Date.now())}] Error Object: %o`, err);
     }
   });
 
   // 1.5 COMPOUND LIQUIDATION CHANNEL
   schedule.scheduleJob({ start: startTime, rule: dailyRule }, async function () {
-    logger.info('-- 🛵 Scheduling Showrunner - Compound Liquidation Channel [on 24 Hours]' + new Date(Date.now()));
+    logger.info(`[${new Date(Date.now())}] -- 🛵 Scheduling Showrunner - Compound Liquidation Channel [on 24 Hours]`);
     const compoundTicker = Container.get(CompoundLiquidationChannel);
     const taskName = 'Compound Liquidation address checks and sendMessageToContract()';
 
     try {
       await compoundTicker.sendMessageToContract(false);
-      logger.info(`🐣 Cron Task Completed -- ${taskName}`);
+      logger.info(`[${new Date(Date.now())}] 🐣 Cron Task Completed -- ${taskName}`);
     }
     catch (err) {
-      logger.error(`❌ Cron Task Failed -- ${taskName}`);
-      logger.error(`Error Object: %o`, err);
+      logger.error(`[${new Date(Date.now())}] ❌ Cron Task Failed -- ${taskName}`);
+      logger.error(`[${new Date(Date.now())}] Error Object: %o`, err);
     }
   });
 
   // 1.6 EVEREST CHANNEL
   schedule.scheduleJob({ start: startTime, rule: dailyRule }, async function () {
-    logger.info('-- 🛵 Scheduling Showrunner - Everest Channel [on 24 Hours]' + new Date(Date.now()));
+    logger.info(`[${new Date(Date.now())}] -- 🛵 Scheduling Showrunner - Everest Channel [on 24 Hours]`);
     const everestTicker = Container.get(Everest);
     const taskName = 'Everest event checks and sendMessageToContract()';
 
     try {
       await everestTicker.sendMessageToContract(false);
-      logger.info(`🐣 Cron Task Completed -- ${taskName}`);
+      logger.info(`[${new Date(Date.now())}] 🐣 Cron Task Completed -- ${taskName}`);
     }
     catch (err) {
-      logger.error(`❌ Cron Task Failed -- ${taskName}`);
-      logger.error(`Error Object: %o`, err);
+      logger.error(`[${new Date(Date.now())}] ❌ Cron Task Failed -- ${taskName}`);
+      logger.error(`[${new Date(Date.now())}] Error Object: %o`, err);
     }
   });
 
   // 1.7 WALLET TRACKER CHANNEL
   schedule.scheduleJob({ start: startTime, rule: thirtyMinuteRule }, async function () {
-    logger.info('-- 🛵 Scheduling Showrunner - Wallet Tracker Channel [on 2.5 Minutes]' + new Date(Date.now()));
+    logger.info(`[${new Date(Date.now())}] -- 🛵 Scheduling Showrunner - Wallet Tracker Channel [on 2.5 Minutes]`);
     const walletTracker = Container.get(WalletTrackerChannel);
     const taskName = 'Track wallets on every new block mined';
 
     try {
       await walletTracker.sendMessageToContract(false);
-      logger.info(`🐣 Cron Task Completed -- ${taskName}`);
+      logger.info(`[${new Date(Date.now())}] 🐣 Cron Task Completed -- ${taskName}`);
     }
     catch (err) {
-      logger.error(`❌ Cron Task Failed -- ${taskName}`);
-      logger.error(`Error Object: %o`, err);
+      logger.error(`[${new Date(Date.now())}] ❌ Cron Task Failed -- ${taskName}`);
+      logger.error(`[${new Date(Date.now())}] Error Object: %o`, err);
     }
   });
 
   // 1.8 HELLO WORLD CHANNEL
   schedule.scheduleJob({ start: startTime, rule: oneHourRule }, async function () {
-    logger.info('-- 🛵 Scheduling Showrunner - HelloWorld Channel [on 1 hour]'  + new Date(Date.now()));
+    logger.info(`[${new Date(Date.now())}] -- 🛵 Scheduling Showrunner - HelloWorld Channel [on 1 hour]`  + new Date(Date.now()));
     const helloTicker = Container.get(HelloWorld);
     const taskName = 'Hello world demo message and sendMessageToContract()';
 
     try {
       await helloTicker.sendMessageToContract(false);
-      logger.info(`🐣 Cron Task Completed -- ${taskName}`);
+      logger.info(`[${new Date(Date.now())}] 🐣 Cron Task Completed -- ${taskName}`);
     }
     catch (err) {
-      logger.error(`❌ Cron Task Failed -- ${taskName}`);
-      logger.error(`Error Object: %o`, err);
+      logger.error(`[${new Date(Date.now())}] ❌ Cron Task Failed -- ${taskName}`);
+      logger.error(`[${new Date(Date.now())}] Error Object: %o`, err);
     }
   });
 
@@ -228,33 +228,33 @@ export default ({ logger }) => {
 
   // 3.1 Wallets Monitoring Service
   schedule.scheduleJob({ start: startTime, rule: oneHourRule }, async function () {
-    logger.info('-- 🛵 Scheduling Showrunner - Wallets Monitoring [every Hour]' + new Date(Date.now()));
+    logger.info(`[${new Date(Date.now())}] -- 🛵 Scheduling Showrunner - Wallets Monitoring [every Hour]`);
     const walletMonitoring = Container.get(WalletMonitoring);
     const taskName = 'WalletMonitoring event checks and processWallet()';
 
     try {
       await walletMonitoring.processWallets(false);
-      logger.info(`🐣 Cron Task Completed -- ${taskName}`);
+      logger.info(`[${new Date(Date.now())}] 🐣 Cron Task Completed -- ${taskName}`);
     }
     catch (err) {
-      logger.error(`❌ Cron Task Failed -- ${taskName}`);
-      logger.error(`Error Object: %o`, err);
+      logger.error(`[${new Date(Date.now())}] ❌ Cron Task Failed -- ${taskName}`);
+      logger.error(`[${new Date(Date.now())}] Error Object: %o`, err);
     }
   });
 
   // 3.2 Main Wallet Monitoring Service
   schedule.scheduleJob({ start: startTime, rule: oneHourRule }, async function () {
-    logger.info('-- 🛵 Scheduling Showrunner - Main Wallets Monitoring [every Hour]' + new Date(Date.now()));
+    logger.info(`[${new Date(Date.now())}] -- 🛵 Scheduling Showrunner - Main Wallets Monitoring [every Hour]`);
     const walletMonitoring = Container.get(WalletMonitoring);
     const taskName = 'Main Wallet Monitoring event checks and processWallet()';
 
     try {
       await walletMonitoring.processMainWallet(false);
-      logger.info(`🐣 Cron Task Completed -- ${taskName}`);
+      logger.info(`[${new Date(Date.now())}] 🐣 Cron Task Completed -- ${taskName}`);
     }
     catch (err) {
-      logger.error(`❌ Cron Task Failed -- ${taskName}`);
-      logger.error(`Error Object: %o`, err);
+      logger.error(`[${new Date(Date.now())}] ❌ Cron Task Failed -- ${taskName}`);
+      logger.error(`[${new Date(Date.now())}] Error Object: %o`, err);
     }
   });
 };
