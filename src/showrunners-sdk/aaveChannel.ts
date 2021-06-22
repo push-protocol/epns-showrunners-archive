b6909315d2e90fa029121ac20cc27eefaff049a3
aiswaryawalter <aiswaryawalter@gmail.com>
2021-06-22 20:36:00 +0530

    update aave-sdk

--- src/showrunners-sdk/aaveChannel.ts
+++ src/showrunners-sdk/aaveChannel.ts
@@ -4,7 +4,7 @@
 import { Service, Inject } from 'typedi';
 import config from '../config';
 import channelWalletsInfo from '../config/channelWalletsInfo';
-import { ethers, logger } from 'ethers';
+import { ethers, logger} from 'ethers';
 import epnsHelper, {InfuraSettings, NetWorkSettings, EPNSSettings} from '../sdk'
 // import epnsHelper, {InfuraSettings, NetWorkSettings} from '@epnsproject/backend-sdk'
 const channelKey = channelWalletsInfo.walletsKV['aavePrivateKey_1']
@@ -23,7 +23,7 @@ const epnsSettings: EPNSSettings = {
   contractAddress: config.deployedContract,
   contractABI: config.deployedContractABI
 }
-let NETWORK_TO_MONIOR = config.web3PolygonMainnetRPC
+const NETWORK_TO_MONIOR = config.web3PolygonMainnetRPC
 const sdk = new epnsHelper(NETWORK_TO_MONIOR, channelKey, settings, epnsSettings)
 
 const HEALTH_FACTOR_THRESHOLD = 1.6;
@@ -36,38 +36,48 @@ const CUSTOMIZABLE_SETTINGS = {
 export default class AaveChannel {
   // To form and write to smart contract
   public async sendMessageToContract(simulate) {
-    // Check simulate object
+    //simulate object settings START
     const logicOverride = typeof simulate == 'object' ? ((simulate.hasOwnProperty("logicOverride") && simulate.logicOverride.mode) ? simulate.logicOverride.mode : false) : false;
     const simulateAaveNetwork = logicOverride && simulate.logicOverride.hasOwnProperty("aaveNetwork") ? simulate.logicOverride.aaveNetwork : false;
     let aave: any;
-    if(logicOverride){
-      if(simulateAaveNetwork){
-        aave = sdk.advanced.getInteractableContracts(simulateAaveNetwork, settings, channelKey, config.aaveLendingPoolDeployedContractPolygonMainnet, config.aaveLendingPoolDeployedContractABI);
-      }
+    if(simulateAaveNetwork){
+      aave = sdk.advanced.getInteractableContracts(simulateAaveNetwork, settings, channelKey, config.aaveLendingPoolDeployedContractPolygonMainnet, config.aaveLendingPoolDeployedContractABI);
     }
     else{
       aave = await sdk.getContract(config.aaveLendingPoolDeployedContractPolygonMainnet, config.aaveLendingPoolDeployedContractABI)
     }
+    //simulate object settings END
+
     const users = await sdk.getSubscribedUsers()
     const promises = users.map(user => this.checkHealthFactor(aave, user, simulate))
     return await Promise.all(promises)
   }
-
   public async checkHealthFactor(aave, userAddress, simulate) {
-    // Check simulate object
+    //simulate object settings START
     const logicOverride = typeof simulate == 'object' ? ((simulate.hasOwnProperty("logicOverride") && simulate.logicOverride.mode) ? simulate.logicOverride.mode : false) : false;
     const simulateApplyToAddr = logicOverride && simulate.logicOverride.hasOwnProperty("applyToAddr") ? simulate.logicOverride.applyToAddr : false;
     const simulateAaveNetwork = logicOverride && simulate.logicOverride.hasOwnProperty("aaveNetwork") ? simulate.logicOverride.aaveNetwork : false;
-    if(simulateApplyToAddr){
-      userAddress = simulateApplyToAddr
+    if(!aave){
+      if(simulateAaveNetwork){
+        aave = sdk.advanced.getInteractableContracts(simulateAaveNetwork, settings, channelKey, config.aaveLendingPoolDeployedContractPolygonMainnet, config.aaveLendingPoolDeployedContractABI);
+      }
+      else{
+        aave = await sdk.getContract(config.aaveLendingPoolDeployedContractPolygonMainnet, config.aaveLendingPoolDeployedContractABI)
+      }
     }
-    if(simulateAaveNetwork){
-      aave = sdk.advanced.getInteractableContracts(simulateAaveNetwork, settings, channelKey, config.aaveLendingPoolDeployedContractPolygonMainnet, config.aaveLendingPoolDeployedContractABI);
+    if(!userAddress){
+      if(simulateApplyToAddr){
+        userAddress = simulateApplyToAddr
+      }
+      else{
+        logger.info('userAddress is not defined')
+      }
     }
+    //simulate object settings END
+      
     const userData = await aave.contract.getUserAccountData(userAddress)
     let  healthFactor = ethers.utils.formatEther(userData.healthFactor)
-    console.log("🚀 ~ file: aaveChannel.ts ~ line 72 ~ AaveChannel ~ checkHealthFactor ~ healthFactor", healthFactor)
-    logger.debug("For wallet: %s, Health Factor: %o", userAddress, healthFactor);
+    logger.info("For wallet: %s, Health Factor: %o", userAddress, healthFactor);
     if(Number(healthFactor) <= HEALTH_FACTOR_THRESHOLD){
       const precision = CUSTOMIZABLE_SETTINGS.precision;
       const newHealthFactor = parseFloat(healthFactor).toFixed(precision);
@@ -75,7 +85,8 @@ export default class AaveChannel {
       const message =  userAddress + " your account has healthFactor "+ newHealthFactor + ". Maintain it above 1 to avoid liquidation.";
       const payloadTitle = "Aave Liquidity Alert!";
       const payloadMsg = `Dear [d:${userAddress}] your account has healthFactor ${newHealthFactor} . Maintain it above 1 to avoid liquidation.[timestamp: ${Math.floor(new Date() / 1000)}]`;
-      const tx = await sdk.sendNotification(userAddress, title, message, payloadTitle, payloadMsg, simulate)
+      const notificationType = 3;
+      const tx = await sdk.sendNotification(userAddress, title, message, payloadTitle, payloadMsg, notificationType, simulate)
       logger.info(tx)
       return{
         success: true,
@@ -83,7 +94,7 @@ export default class AaveChannel {
       }
     }
     else{
-      logger.debug("Wallet: %s is SAFE with Health Factor: %o", userAddress, healthFactor);
+      logger.info("Wallet: %s is SAFE with Health Factor: %o", userAddress, healthFactor);
       return{
         success: false,
         data: userAddress + " is not about to get liquidated"
--- src/showrunners/aaveChannel.ts
+++ src/showrunners/aaveChannel.ts
@@ -8,8 +8,8 @@ import { EventDispatcher, EventDispatcherInterface } from '../decorators/eventDi
 import { ethers } from 'ethers';
 import epnsNotify from '../helpers/epnsNotifyHelper';
 
-const NETWORK_TO_MONITOR = config.web3PolygonMumbaiRPC;
-const HEALTH_FACTOR_THRESHOLD = 1.1;
+const NETWORK_TO_MONITOR = config.web3PolygonMainnetRPC;
+const HEALTH_FACTOR_THRESHOLD = 1.6;
 const CUSTOMIZABLE_SETTINGS = {
   'precision': 3,
 }
@@ -97,93 +97,93 @@ export default class AaveChannel {
       const aave = this.getAaveInteractableContract(networkToMonitor);
       
 
-      // epns.contract.channels(aaveChannelAddress)
-      // .then(async (channelInfo) => {
+      epns.contract.channels(aaveChannelAddress)
+      .then(async (channelInfo) => {
 
-      //   const filter = epns.contract.filters.Subscribe(aaveChannelAddress)
+        const filter = epns.contract.filters.Subscribe(aaveChannelAddress)
 
-      //   let startBlock = channelInfo.channelStartBlock.toNumber();
+        let startBlock = channelInfo.channelStartBlock.toNumber();
 
-      //   //Function to get all the addresses in the channel
-      //   epns.contract.queryFilter(filter, startBlock)
-      //   .then(async (eventLog) => {
-      //     // Log the event
-      //     logger.debug("Event log returned %o", eventLog);
+        //Function to get all the addresses in the channel
+        epns.contract.queryFilter(filter, startBlock)
+        .then(async (eventLog) => {
+          // Log the event
+          logger.debug("Event log returned %o", eventLog);
 
-      //     // Loop through all addresses in the channel and decide who to send notification
-      //     let allTransactions = [];
+          // Loop through all addresses in the channel and decide who to send notification
+          let allTransactions = [];
 
-      //     eventLog.map((log) => {
-      //       // Get user address
-      //       const userAddress = log.args.user;
-      //       logger.debug("🚀 ~ file: aaveChannel.ts ~ line 91 ~ AaveChannel ~ eventLog.map ~ userAddress %o", userAddress)
-      //       allTransactions.push(
-      //         this.checkHealthFactor(aave, NETWORK_TO_MONITOR, userAddress, simulate)
-      //           .then( (results) => {
-      //             return results;
-      //           })
-      //       );
-      //     })
+          eventLog.map((log) => {
+            // Get user address
+            const userAddress = log.args.user;
+            logger.debug("🚀 ~ file: aaveChannel.ts ~ line 91 ~ AaveChannel ~ eventLog.map ~ userAddress %o", userAddress)
+            allTransactions.push(
+              this.checkHealthFactor(aave, NETWORK_TO_MONITOR, userAddress, simulate)
+                .then( (results) => {
+                  return results;
+                })
+            );
+          })
 
-      //     // resolve all transactions
-      //     Promise.all(allTransactions)
-      //     .then(async (results) => {
-      //       logger.debug("All Transactions Loaded: %o", results);
-      //       for (const object of results) {
-      //         if (object.success) {
-      //           // Send notification
-      //           const wallet = object.wallet;
-      //           const ipfshash = object.ipfshash;
-      //           const payloadType = object.payloadType;
+          // resolve all transactions
+          Promise.all(allTransactions)
+          .then(async (results) => {
+            logger.debug("All Transactions Loaded: %o", results);
+            for (const object of results) {
+              if (object.success) {
+                // Send notification
+                const wallet = object.wallet;
+                const ipfshash = object.ipfshash;
+                const payloadType = object.payloadType;
 
-      //         logger.info("Wallet: %o | Hash: :%o | Sending Data...", wallet, ipfshash);
+              logger.info("Wallet: %o | Hash: :%o | Sending Data...", wallet, ipfshash);
 
-      //         const storageType = 1; // IPFS Storage Type
-      //         const txConfirmWait = 1; // Wait for 0 tx confirmation
+              const storageType = 1; // IPFS Storage Type
+              const txConfirmWait = 1; // Wait for 0 tx confirmation
 
-      //         // Send Notification
-      //         await epnsNotify.sendNotification(
-      //           epns.signingContract,                                           // Contract connected to signing wallet
-      //           wallet,                                                         // Recipient to which the payload should be sent
-      //           payloadType,                                                    // Notification Type
-      //           storageType,                                                    // Notificattion Storage Type
-      //           ipfshash,                                                       // Notification Storage Pointer
-      //           txConfirmWait,                                                  // Should wait for transaction confirmation
-      //           logger,                                                        // Logger instance (or console.log) to pass
-      //           simulate                                                        // Passing true will not allow sending actual notification
-      //         ).then ((tx) => {
-      //           logger.info("Transaction mined: %o | Notification Sent", tx.hash);
-      //           resolve(tx);
-      //         })
-      //         .catch (err => {
-      //           logger.error("🔥Error --> sendNotification(): %o", err);
-      //           reject(err);
-      //         });
-      //         }
-      //       }
-      //       logger.debug("Aave Liquidation Alert! logic completed!");
-      //     })
-      //     .catch(err => {
-      //       logger.error("Error occurred sending transactions: %o", err);
-      //       reject(err);
-      //     });
-      //     resolve("Processing Aave Liquidation Alert! logic completed!");
-      //   })
-      //   .catch(err => {
-      //     logger.error("Error occurred while looking at event log: %o", err);
-      //     reject(err);
-      //   })
-      // })
-      // .catch(err => {
-      //   logger.error("Error retreiving channel start block: %o", err);
-      //   reject(err);
-      // });
+              // Send Notification
+              await epnsNotify.sendNotification(
+                epns.signingContract,                                           // Contract connected to signing wallet
+                wallet,                                                         // Recipient to which the payload should be sent
+                payloadType,                                                    // Notification Type
+                storageType,                                                    // Notificattion Storage Type
+                ipfshash,                                                       // Notification Storage Pointer
+                txConfirmWait,                                                  // Should wait for transaction confirmation
+                logger,                                                        // Logger instance (or console.log) to pass
+                simulate                                                        // Passing true will not allow sending actual notification
+              ).then ((tx) => {
+                logger.info("Transaction mined: %o | Notification Sent", tx.hash);
+                resolve(tx);
+              })
+              .catch (err => {
+                logger.error("🔥Error --> sendNotification(): %o", err);
+                reject(err);
+              });
+              }
+            }
+            logger.debug("Aave Liquidation Alert! logic completed!");
+          })
+          .catch(err => {
+            logger.error("Error occurred sending transactions: %o", err);
+            reject(err);
+          });
+          resolve("Processing Aave Liquidation Alert! logic completed!");
+        })
+        .catch(err => {
+          logger.error("Error occurred while looking at event log: %o", err);
+          reject(err);
+        })
+      })
+      .catch(err => {
+        logger.error("Error retreiving channel start block: %o", err);
+        reject(err);
+      });
     })
   }
 
   public async getHealthFactor(aave, networkToMonitor, userAddress, simulate){
     // Check simulate object
-    const logicOverride = typeof simulate == 'object' ? (simulate.hasOwnProperty("logicOverride") ? simulate.hasOwnProperty("logicOverride") : false) : false;
+    const logicOverride = typeof simulate == 'object' ? (simulate.hasOwnProperty("logicOverride")  && simulate.logicOverride.mode ? simulate.logicOverride.mode : false) : false;
     const mode = logicOverride && simulate.logicOverride.mode ? simulate.logicOverride.mode : false;
     const simulateApplyToAddr = logicOverride && simulate.logicOverride.hasOwnProperty("applyToAddr") ? simulate.logicOverride.applyToAddr : false;
     const simulateAaveNetwork = logicOverride && simulate.logicOverride.hasOwnProperty("aaveNetwork") ? simulate.logicOverride.aaveNetwork : false;
